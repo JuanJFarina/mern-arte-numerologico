f367034fa1925e5466b5aeef555b62bea81d4121
Juan Jose Farina <juanjosefarina.jjf@gmail.com>
2023-06-06 16:02:13 -0300

    changed bcrypt for bcryptjs

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs'); // Use bcryptjs instead of bcrypt
 const bodyParser = require('body-parser');
 
 const userSchema = new mongoose.Schema({
