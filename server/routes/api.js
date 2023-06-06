b6bc4e29c8cdaf30b4e19344b1f6c5f9c2dccfcc
Juan Jose Farina <juanjosefarina.jjf@gmail.com>
2023-06-06 14:48:12 -0300

    readded body parser

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+let bodyParser = require('body-parser');
 
 const userSchema = new mongoose.Schema({
   useremail: {
