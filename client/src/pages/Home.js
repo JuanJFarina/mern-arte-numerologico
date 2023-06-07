6f400b9004005e8441a4e8b07a1436f7b31bd744
Juan Jose Farina <juanjosefarina.jjf@gmail.com>
2023-06-07 16:32:32 -0300

    added sidebar

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import Sidebar from '../components/Sidebar.js';
 
 export default function Home() {
   return (
     <main className="container-fluid">
       <div className="row">
-        <div className="col-1 col-sm-2"></div>
+        <div className="col-1 col-sm-2">
+          <Sidebar />
+        </div>
         <div className="col-10 col-sm-8">
-          <h1>¿ Qué es la Numerología ?</h1>
+          <h1 id="section0">¿ Qué es la Numerología ?</h1>
           <p>La numerología es una antigua disciplina que busca asociar un significado a un concepto abstracto relativo a algún número.
             Esto deriva en aplicaciones muy variadas utilizando nombres, nacimientos, fechas, direcciones y más.
             Para entender la Numerología es necesario comprender el significado abstracto de los números, por éso vamos a ver una breve definición de cada uno:</p>
-          <h2>El número 1</h2>
+          <h2 id="section1">El número 1</h2>
           <p>El número uno indica tanto la individualidad como la grupalidad.
             Se refiere a los comportamientos como "unidad", al "todo", y a los sentimientos asociados a los mismos.</p>
           <dl>
@@ -24,7 +27,7 @@ export default function Home() {
               <dd>Tiranía</dd>
               <dd>Intolerancia</dd>
           </dl>
-          <h2>El Número 2</h2>
+          <h2 id="section2">El Número 2</h2>
           <p>El número dos representa la dualidad, el contraste, la rivalidad y la oposición.</p>
           <dl>
             <dt>Aspectos positivos</dt>
@@ -37,7 +40,7 @@ export default function Home() {
               <dd>Cobardía</dd>
               <dd>Decepción</dd>
           </dl>
-          <h2>El Número 3</h2>
+          <h2 id="section3">El Número 3</h2>
           <p>El tres nace de la suma/asociación del 1 y el 2, simbolizando la tríada inicio, centro, final.</p>
           <dl>
             <dt>Aspectos positivos</dt>
@@ -50,7 +53,7 @@ export default function Home() {
               <dd>Celos</dd>
               <dd>Hipocresía</dd>
           </dl>
-          <h2>El Número 4</h2>
+          <h2 id="section4">El Número 4</h2>
           <p>El número cuatro representa el nuevo inicio (1) después del primer fin (3), simbolizado por el cuadrado y asociado a lo ideal y correcto.</p>
           <dl>
             <dt>Aspectos positivos</dt>
@@ -63,7 +66,7 @@ export default function Home() {
               <dd>Violencia</dd>
               <dd>Crueldad</dd>
           </dl>
-          <h2>El Número 5</h2>
+          <h2 id="section5">El Número 5</h2>
           <p>El cinco está asociado a los sentidos y las experiencias.</p>
           <dl>
             <dt>Aspectos positivos</dt>
@@ -76,7 +79,7 @@ export default function Home() {
               <dd>Perversión</dd>
               <dd>Malevolencia</dd>
           </dl>
-          <h2>El Número 6</h2>
+          <h2 id="section6">El Número 6</h2>
           <p>Es el número de la armonía y la naturaleza simbolizado por el hexágono.</p>
           <dl>
             <dt>Aspectos positivos</dt>
@@ -89,7 +92,7 @@ export default function Home() {
               <dd>Cinismo</dd>
               <dd>Tiranía</dd>
           </dl>
-          <h2>El Número 7</h2>
+          <h2 id="section7">El Número 7</h2>
           <p>Es el número que está por encima de lo natural (6), asociado a lo divino y al tiempo.</p>
           <dl>
             <dt>Aspectos positivos</dt>
@@ -102,7 +105,7 @@ export default function Home() {
               <dd>Deshonestidad</dd>
               <dd>Infidelidad</dd>
           </dl>
-          <h2>El Número 8</h2>
+          <h2 id="section8">El Número 8</h2>
           <p>El ocho es también el símbolo del infinito y es la figura del equilibrio y de la integración de todos los contrastes y posibilidades.</p>
           <dl>
             <dt>Aspectos positivos</dt>
@@ -115,7 +118,7 @@ export default function Home() {
               <dd>Opresión</dd>
               <dd>Injusticia</dd>
           </dl>
-          <h2>El Número 9</h2>
+          <h2 id="section9">El Número 9</h2>
           <p>El nueve es el último proceso humano dentro de la numerología, culminando una triple triada (3x3) de estados evolutivos.</p>
           <dl>
             <dt>Aspectos positivos</dt>
@@ -128,7 +131,7 @@ export default function Home() {
               <dd>Inmoralidad</dd>
               <dd>Amargura</dd>
           </dl>
-          <h2>El Número 0</h2>
+          <h2 id="section00">El Número 0</h2>
           <p>El cero significa lo eterno, el universo, y el crecimiento potencial del que evolucionan los nueve dígitos.
             Es un símbolo inrepresentable en lo terrenal y, por tanto, se lo asocia a una fuerza creadora superior.</p>
         </div>
