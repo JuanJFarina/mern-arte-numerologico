31263b9bd812d7ba15d8e96304763945ecee7b1f
Juan Jose Farina <juanjosefarina.jjf@gmail.com>
2023-06-16 20:59:37 -0300

    deleted everything cors related

--- server/index.js
+++ server/index.js
@@ -1,16 +1,5 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
-app.use(cors());
-app.options('/api/*', (req, res) => {
-  // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
-
-  // Respond with a 200 status code
-  res.sendStatus(200);
-});
 const apiRoutes = require('./routes/api.js');
 
 const mongoose = require('mongoose');
