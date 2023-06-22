60ce744bcc55e1801b18621799a6ac68298cf74b
Juan José Farina <juanjosefarina.jjf@gmail.com>
2023-06-22 01:02:58 -0300

    escaped special characters in url

--- server/routes/cryptAlgo.js
+++ server/routes/cryptAlgo.js
@@ -1,11 +1,16 @@
 function crypt(email) {
-    let cryptedEmail = '3a.1B4c1D5e9F2g6H4i3J8k2L6m4N3o3P8q4R6s2T6u4V3w3X8y4Z6a5B9c7D9e3F2g0';
-    return cryptedEmail+email;
+    let cryptedEmail = '3a1B4c1D5e9F2g6H4i3J8k2L6m4N3o3P8q4R6s2T6u4V3w3X8y4Z6a5B9c7D9e3F2g0';
+    const noDots = email.replace('.', '%2E');
+    const noArroba = noDots.replace('@', '%40');
+    return cryptedEmail+noArroba;
 }
 
 function decrypt(cryptedEmail) {
     const splited = cryptedEmail.split('c7D9e3F2g0');
-    return splited[1];
+    const uncrypted = splited[1];
+    const withArroba = uncrypted.replace('%40', '@');
+    const withDots = withArroba.replace('%2E', '.');
+    return withDots;
 }
 
 module.exports = { crypt, decrypt };
